4bf3a3f30809b4a04a48485073589889c1c679cc
hemantChandalia <154237393+hemantChandalia@users.noreply.github.com>
2023-12-28 19:59:01 +0530

    crypto currency website

---
+++ Cryptocurrency Website Design/script.js
@@ -0,0 +1,29 @@
+var btc__ = document.getElementById("bitcoin");
+var eth__ = document.getElementById("ethereum");
+var doge__ = document.getElementById("dogecoin");
+
+var settings =  {
+    "async" : true,
+    "scrossDomain": true,
+    "url": "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Cdogecoin&vs_currencies=inr&x_cg_demo_api_key=CG-f5HryTkMcUF2jerwxhY5v6AY",
+    "method": "GET",    
+    "headers": {}
+}
+$.ajax(settings).done(function (response){
+
+    // JavaScript code to format the number
+const btc = response.bitcoin.inr;
+const eth = response.ethereum.inr;
+const doge = response.dogecoin.inr; // Your number goes here
+
+// Format the number with commas for thousands separator
+btc__.innerHTML = btc.toLocaleString('en-US', { style: 'currency', currency: 'INR' });
+eth__.innerHTML = eth.toLocaleString('en-US', { style: 'currency', currency: 'INR' });
+doge__.innerHTML = doge.toLocaleString('en-US', { style: 'currency', currency: 'INR' });
+
+console.log( btc.toLocaleString('en-US', { style: 'currency', currency: 'INR' }))
+
+});
+
+
+// https://api.coingecko.com/api/v3/ping?
---
+++ Image Search App/script.js
@@ -0,0 +1,75 @@
+// loader 
+// enter button activtion
+// light box for photos open together
+// photo clickable
+// random photos at home page
+//  more photos at home page instead of 3
+// adding better css
+// header >> images keep repating like Pexel
+// navbar
+// search button attractivness
+// make website like Pexel and Yandex
+
+const accessKey = "4CW4fmD5UhhB1SumLRtm9zLfdUamebyFKmFf12uZq0U"
+
+const formEl = document.querySelector("form")
+const inputEl = document.getElementById("search-input")
+const searchResults = document.querySelector(".search-results")
+const showMore = document.getElementById("show-more-button")
+
+let inputDate = ""
+let page = 1;
+
+async function searchImages() {
+    inputDate = inputEl.value;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputDate}&client_id=${accessKey}`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    const results = data.results;
+
+    if (page === 1) {
+        searchResults.innerHTML = "";
+
+    }
+
+    results.map((result) => {
+        const imageWrapper = document.createElement("div");
+        imageWrapper.classList.add("search-result");
+        const image = document.createElement('img');
+        image.src = result.urls.small;
+        image.alt = result.alt_description;
+        const imageLink = document.createElement('a');
+        imageLink.href = result.links.html;
+        // document.imageLink.style.searchresult.backgroundColor = "red";
+        imageLink.target = "_blank";
+        imageLink.textContent = result.alt_description;
+        // >>>>>>>>>>*********<<<<<<>>>>>>>>
+
+        // document.getElementById("anchorcolor").style.backgroundColor = "red";
+
+
+
+        imageWrapper.appendChild(image);
+        imageWrapper.appendChild(imageLink);
+        searchResults.appendChild(imageWrapper);
+    })
+    page++
+
+    if (page > 1) {
+        showMore.style.display = "block"
+    }
+}
+
+
+
+formEl.addEventListener("submit", (event) => {
+    event.preventDefault()
+    page = 1;
+    searchImages()
+})
+
+showMore.addEventListener("click", () => {
+    searchImages()
+})
